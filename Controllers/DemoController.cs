ec1fee56188f98c627610b2decda4f3de9e6376b
aditi.tekale <aditi.tekale@paypointindia.net>
2026-09-16 16:00:38 +0530

    This is the 2nd commit

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Test.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index_View2()
+        {
+            int var = 10;
+            return View();
+        }
+       
+    }
+}
